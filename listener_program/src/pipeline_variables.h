d12fda5ed178a5f2f3b8cd2535d9a57a46c570e2
Joe <jlwalker@ucsd.edu>
2025-03-17 11:47:52 -0700

    updating readme and not ignoring build

---
+++ listener_program/src/pipeline_variables.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "pch.h"
+
+struct PipelineVariables
+{
+    std::chrono::seconds clusterFrequencyInSeconds;
+    std::chrono::seconds clusterWindowInSeconds;
+
+    float timeDomainThreshold = 0;
+    float energyDetectionThreshold = 0;
+    float speedOfSound = 0;
+
+    bool integrationTesting = false;
+    bool enableTracking = false;
+    bool useImu = false;
+
+    std::string loggingDirectory = "";
+    std::string timeDomainDetector = "";
+    std::string frequencyDomainDetector = "";
+    std::string frequencyDomainStrategy = "";
+    std::string filterWeightsPath = "";
+    std::string receiverPositionsPath = "";
+    std::string onnxModelPath = "";
+    std::string onnxModelNormalizationPath = "";
+};
---
+++ listener_program/src/socket_variables.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "pch.h"
+
+struct SocketVariables
+{
+    std::string ipAddress = "";
+    int port = -1;
+};
